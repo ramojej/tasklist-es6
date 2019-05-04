cea5c5c0f67bbfb01e2fa9fc140d523473474c36
Jejo <jejomar@im.skubbs.com>
2019-05-04 14:44:43 +0800

    Added clear task, delete task, and filter function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Define ui vars
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.cleark-tasks');
+const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task'); 
 
@@ -11,6 +11,15 @@ loadEventListeners();
 function loadEventListeners() {
     //add task event
     form.addEventListener('submit', addTask);
+
+    //remove task event
+    taskList.addEventListener('click', removeTask);
+
+    //clear task event
+    clearBtn.addEventListener('click', clearTasks);
+
+    //filter tasks events
+    filter.addEventListener('keyup', filterTasks);
 }
 
 //add Task
@@ -42,3 +51,37 @@ function addTask(e) {
 
     e.preventDefault();
 }
+
+//remove task
+function removeTask(e) {
+    if( e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+//clear task
+function clearTasks() {
+    //taskList.innerHTML = '';
+    if (confirm('Are you sure you want to clear all tasks?')) {
+        while(taskList.firstChild) {
+            taskList.removeChild(taskList.firstChild);
+        }
+    }
+}
+
+//filter task
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(task) {
+         const item = task.firstChild.textContent;
+         if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+         } else {
+             task.style.display = 'none';
+         }
+    });
+}
+    
